523ffb42d9cb4d7d1c60160015a93e88afd1660f
ciross1 <oricsantamarina@gmail.com>
2026-05-30 21:46:50 +0200

    Login page test 2 verbessert

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,5 +1,10 @@
 import com.myself.pages.LoginPage;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LoginTest extends BaseTest{
 
@@ -12,7 +17,17 @@ public class LoginTest extends BaseTest{
         loginPage.enterPassword("admin");
         loginPage.clickLoginButton();
 
+
+
         System.out.println("Title of the page: " + driver.getTitle());
+
+        // Hier wird geprüft, ob die aktuelle URL das Wort "login" enthält.
+        // Dieser Ausdruck muss true sein, sonst schlägt der Test fehl
+        Assertions.assertTrue(
+                driver.getCurrentUrl().contains("login"),
+                "Bei falschem Login sollte man auf der Login-Seite bleiben"
+        );
+
 
     }
 
